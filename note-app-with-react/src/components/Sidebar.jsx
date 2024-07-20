769f7157cb63a9166144d976d154c367b99a8b5c
midorigameee <xqbodp727@gmail.com>
2024-07-20 08:09:27 +0000

    Sidebarの中身を作成

--- note-app-with-react/src/components/Sidebar.jsx
+++ note-app-with-react/src/components/Sidebar.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>note</h1>
+        <button>add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note"></div>
+        <div className="sidebar-note-title">
+          <strong>title</strong>
+          <button>delete</button>
+        </div>
+        <p>note contents</p>
+        <small>last modified: MM/dd</small>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
