9837fd68c5f6dfb37338aff3dc6b6cabf86076a5
midorigameee <xqbodp727@gmail.com>
2024-07-21 16:07:03 +0000

    マークダウンプラグインを追加

--- note-app-with-react/src/components/Main.jsx
+++ note-app-with-react/src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Main.css";
+import Markdown from "react-markdown";
+// import { ReactMarkDown }
 
 const Main = ({ activeNote, onUpdateNote }) => {
   const onEditNote = (key, value) => {
@@ -38,7 +40,7 @@ const Main = ({ activeNote, onUpdateNote }) => {
       </div>
       <div className="app-main-note-preview">
         <h1 className="preview-title">{activeNote.title}</h1>
-        <div className="markdown-preview">{activeNote.content}</div>
+        <Markdown className="markdown-preview">{activeNote.content}</Markdown>
       </div>
     </div>
   );
